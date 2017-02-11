97a9ecbb95f15e490ecb90c65d4b86226041335d
Kuba Miszcz <zielonyeufor@gmail.com>
2017-02-11 15:33:38 +0100

    upgrady

--- dolcenapln/About.Designer.cs
+++ dolcenapln/About.Designer.cs
@@ -28,8 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -37,7 +39,7 @@
             this.button1.BackColor = System.Drawing.Color.Red;
             this.button1.BackgroundImage = global::dolcenapln.Properties.Resources.wirus;
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button1.Location = new System.Drawing.Point(33, 87);
+            this.button1.Location = new System.Drawing.Point(91, 247);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(84, 85);
@@ -48,13 +50,23 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Location = new System.Drawing.Point(71, 8);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(138, 65);
+            this.label1.Size = new System.Drawing.Size(138, 91);
             this.label1.TabIndex = 1;
-            this.label1.Text = "zamiana USD na PLN ajwaj\r\n\r\n Kuba Miszcz 2015\r\n\r\nbombowo;]";
+            this.label1.Text = "zamiana USD na PLN ajwaj\r\n\r\n Kuba Miszcz 2015\r\n\r\nbąbowo;]\r\n\r\nInstrukcja:";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 107);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(258, 130);
+            this.label2.TabIndex = 2;
+            this.label2.Text = resources.GetString("label2.Text");
             // 
             // About
             // 
@@ -62,8 +74,9 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Red;
-            this.ClientSize = new System.Drawing.Size(155, 184);
+            this.ClientSize = new System.Drawing.Size(274, 340);
             this.ControlBox = false;
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -80,5 +93,6 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- readme.md
+++
@@ -1,4 +0,0 @@
-dorob przycisk zawsze na wierzchuna wierzchu np ikonka pinezka
-dorob zapis do pliku tej wartosci dolca
-doorb zwijanie do paska pzycisk
-zwez pola i doorb liczbe sztuk obok i ile za sztuke
